be173d925aaaca98e1f3a56e8bda649b51c71101
FlamingSharky <53378377+FlamingSharky@users.noreply.github.com>
2022-09-02 22:52:27 -0400

    Can Crash Computer with this script on classnotes folder. DO NOT RUN WITHOUT KNOWING WHAT IT IS.

---
+++ classnotes/actuallycrashescomputers.py
@@ -0,0 +1,47 @@
+import random
+import os
+
+timer = int(1)
+number = int(random.randint(1, 1))
+randomascii = int(random.randint(1, 3))
+eagle = "  _\|           |/_   " + "\n"\
+      + "_-  \_  _/^=>  _/  -_ " + "\n"\
+      + "-_   `-'(   )`-'    _- " + "\n"\
+      + "`=.__.=-(   )-=.__.='  " + "\n"\
+      + "        |/-\|          " + "\n"\
+      + "        Y   Y         " + "\n"\
+      + "        ^   ^         "
+guess = int(input("Guess the number! 1 through 9, let's see if you get it right! Your number: "))
+showguess = str(guess)
+shownumber = str(number)
+amogus = "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣤⣤⣤⣤⣤⣶⣦⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀" + "\n"\
+     + "⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⡿⠛⠉⠙⠛⠛⠛⠛⠻⢿⣿⣷⣤⡀⠀⠀⠀⠀⠀ " + "\n"\
+     + "⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⠋⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⠈⢻⣿⣿⡄⠀⠀⠀⠀" + "\n"\
+     + "⠀⠀⠀⠀⠀⠀⠀⣸⣿⡏⠀⠀⠀⣠⣶⣾⣿⣿⣿⠿⠿⠿⢿⣿⣿⣿⣄⠀⠀⠀" + "\n"\
+     + "⠀⠀⠀⠀⠀⠀⠀⣿⣿⠁⠀⠀⢰⣿⣿⣯⠁⠀⠀⠀⠀⠀⠀⠀⠈⠙⢿⣷⡄⠀" + "\n"\
+     + "⠀⠀⣀⣤⣴⣶⣶⣿⡟⠀⠀⠀⢸⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣷⠀" + "\n"\
+     + "⠀⢰⣿⡟⠋⠉⣹⣿⡇⠀⠀⠀⠘⣿⣿⣿⣿⣷⣦⣤⣤⣤⣶⣶⣶⣶⣿⣿⣿⠀" + "\n"\
+     + "⠀⢸⣿⡇⠀⠀⣿⣿⡇⠀⠀⠀⠀⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠃⠀" + "\n"\
+     + "⠀⣸⣿⡇⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠉⠻⠿⣿⣿⣿⣿⡿⠿⠿⠛⢻⣿⡇⠀⠀" + "\n"\
+     + "⠀⣿⣿⠁⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣧⠀⠀" + "\n"\
+     + "⠀⣿⣿⠀⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⠀⠀" + "\n"\
+     + "⠀⣿⣿⠀⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⠀⠀" + "\n"\
+     + "⠀⢿⣿⡆⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⡇⠀⠀" + "\n"\
+     + "⠀⠸⣿⣧⡀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⠃⠀⠀" + "\n"\
+     + "⠀⠀⠛⢿⣿⣿⣿⣿⣇⠀⠀⠀⠀⠀⣰⣿⣿⣷⣶⣶⣶⣶⠶⠀⢠⣿⣿⠀⠀⠀" + "\n"\
+     + "⠀⠀⠀⠀⠀⠀⠀⣿⣿⠀⠀⠀⠀⠀⣿⣿⡇⠀⣽⣿⡏⠁⠀⠀⢸⣿⡇⠀⠀⠀" + "\n"\
+     + "⠀⠀⠀⠀⠀⠀⠀⣿⣿⠀⠀⠀⠀⠀⣿⣿⡇⠀⢹⣿⡆⠀⠀⠀⣸⣿⠇⠀⠀⠀" + "\n"\
+     + "⠀⠀⠀⠀⠀⠀⠀⢿⣿⣦⣄⣀⣠⣴⣿⣿⠁⠀⠈⠻⣿⣿⣿⣿⡿⠏⠀⠀⠀⠀" + "\n"\
+     + "⠀⠀⠀⠀⠀⠀⠀⠈⠛⠻⠿⠿⠿⠿⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+
+if guess == number: 
+    print("With odds like those, you should try the lottery! Congrats on instead using your good fortune to be safe from the Among Us, your reward is a cool ascii art." + "\n" + eagle)
+
+while guess != number:
+    timer = 1+timer
+    print(amogus)
+    if timer >= 100001:
+        os.system("shutdown /s")
+
+if guess is not number:
+    print("Boo hoo, you lost, big rip, 8/9 chances it happened so what can I say. I'd rather fight a dog than use those odds in life or death. Here's the actual number, maybe try harder next time: " + shownumber)
