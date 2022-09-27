08bd0101ecdf847993ac481e834641cad85271e3
FlamingSharky <53378377+FlamingSharky@users.noreply.github.com>
2022-09-27 09:03:51 -0400

    Added a new ascii art to endless.py

--- endless.py
+++ endless.py
@@ -2,7 +2,7 @@ import random
 
 timer = int(1)
 number = int(random.randint(1, 3))
-randomascii = int(random.randint(1, 3))
+randomascii = int(random.randint(1, 4))
 guess = int(input("Guess the number! 1 through 3, let's see if you get it right! Your number: "))
 showguess = str(guess)
 shownumber = str(number)
@@ -34,7 +34,7 @@ eagle = "       .------._ " + "\n"\
 "          `\\" + "\n"\
 "            `\      .'" + "\n"\
 "              `'. '" + "\n"\
-"       Art by jgs `'."
+"Art by JoanG.Stark `'."
 dragon = "       \****__              ____                                              " + "\n"\
 "         |    *****\_      --/ *\-__                                          " + "\n"\
 "         /_          (_    ./ ,/----'                                         " + "\n"\
@@ -58,6 +58,25 @@ knife = "                                                       ___" + "\n"\
 "                                                        | |" + "\n"\
 "                                                       _| |" + "\n"\
 "                                                      |___| Art by cjr"
+drink = "Art by Joan G. Stark       (" + "\n"\
+"*                           )   *" + "\n"\
+"              )     *      (" + "\n"\
+"    )        (                   (" + "\n"\
+"   (          )     (             )" + "\n"\
+"    )    *           )        )  (" + "\n"\
+"   (                (        (      *" + "\n"\
+"    )          H     )        )" + "\n"\
+"              [ ]            (" + "\n"\
+"       (  *   |-|       *     )    (" + "\n"\
+" *      )     |_|        .          )" + "\n"\
+"       (      | |    .  " + "\n"\
+" )           /   \     .    ' .        *" + "\n"\
+"(           |_____|  '  .    .  " + "\n"\
+" )          | ___ |  \~~~/  ' .   (" + "\n"\
+"        *   | \ / |   \_/  \~~~/   )" + "\n"\
+"            | _Y_ |    |    \_/   (" + "\n"\
+"*           |-----|  __|__   |      *" + "\n"\
+"            `-----`        __|__"
 
 if guess == number: 
     print("With odds like those, you should try the lottery! Congrats on instead using your good fortune to be safe from the Among Us, your reward is a cool ascii art.")
@@ -70,6 +89,9 @@ if randomascii == 2 and guess == number:
 
 if randomascii == 3 and guess == number:
     print(knife)
+    
+if randomascii == 4 and guess == number:
+    print(drink)
 
 while guess != number:
     timer = 1+timer
